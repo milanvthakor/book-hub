c6f4cd36865691a5faf329a3bbc4e43f979c3c28
Milan Thakor <65599600+milanvthakor@users.noreply.github.com>
2023-09-06 09:27:25 +0530

    03 - Book Details (#3)
    * Implement endpoint to get book details by id

--- api/controllers/books.js
+++ api/controllers/books.js
@@ -1,3 +1,4 @@
+const { default: mongoose } = require('mongoose');
 const Book = require('../models/book');
 
 module.exports.books = async (req, res, next) => {
@@ -15,6 +16,29 @@ module.exports.books = async (req, res, next) => {
             count: books.length,
             books: books
         });
+    } catch (err) {
+        next(err);
+    }
+}
+
+module.exports.bookDetails = async (req, res, next) => {
+    const id = req.params.id;
+
+    if (!mongoose.Types.ObjectId.isValid(id)) {
+        const err = new Error('Invalid id');
+        err.status = 400;
+        return next(err);
+    }
+
+    try {
+        const book = await Book.findById(id).select('-__v');
+        if (!book) {
+            const err = new Error('No book found with given id');
+            err.status = 404;
+            return next(err);
+        }
+
+        res.status(200).json(book);
     } catch (err) {
         next(err);
     }
--- api/routes/books.js
+++ api/routes/books.js
@@ -4,5 +4,6 @@ const router = express.Router();
 const BooksController = require('../controllers/books');
 
 router.get('/', BooksController.books);
+router.get('/:id', BooksController.bookDetails);
 
 module.exports = router;
