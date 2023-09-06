7ff3080d6af4bac2330e8c0ceefb67e50d049138
Milan Thakor <milanv.thakor@gmail.com>
2023-09-06 09:14:49 +0530

    Implement endpoint to get book details by id

--- api/controllers/books.js
+++ api/controllers/books.js
@@ -1,3 +1,4 @@
+const { default: mongoose } = require('mongoose');
 const Book = require('../models/book');
 
 module.exports.books = async (req, res, next) => {
@@ -15,6 +16,27 @@ module.exports.books = async (req, res, next) => {
             count: books.length,
             books: books
         });
+    } catch (err) {
+        next(err);
+    }
+}
+
+module.exports.bookDetails = async (req, res, next) => {
+    const id = req.params.id;
+
+    if (!mongoose.Types.ObjectId.isValid(id)) {
+        const err = new Error('Invalid id');
+        return next(err);
+    }
+
+    try {
+        const book = await Book.findById(id).select('-__v');
+        if (!book) {
+            const err = new Error('No book found with given id');
+            return next(err);
+        }
+
+        res.status(200).json(book);
     } catch (err) {
         next(err);
     }
