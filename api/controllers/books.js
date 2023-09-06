0d7aba55e2190af99e21351e5cb099ece8df68f1
Milan Thakor <milanv.thakor@gmail.com>
2023-09-06 09:00:30 +0530

    Implement search functionality for books

--- api/controllers/books.js
+++ api/controllers/books.js
@@ -1,8 +1,16 @@
 const Book = require('../models/book');
 
 module.exports.books = async (req, res, next) => {
+    const search = req.query.search || "";
+
     try {
-        const books = await Book.find().select('-__v');
+        const books = await Book.find({
+            '$or': [
+                { title: { '$regex': search, '$options': 'i' } },
+                { author: { '$regex': search, '$options': 'i' } }
+            ]
+        }).select('-__v');
+
         res.status(200).json({
             count: books.length,
             books: books
--- api/models/book.js
+++ api/models/book.js
@@ -3,6 +3,7 @@ const mongoose = require('mongoose');
 const bookSchema = mongoose.Schema({
     _id: mongoose.Schema.Types.ObjectId,
     title: { type: String, required: true },
+    isbn: { type: String, required: true, unique: true },
     author: String,
     genre: String,
     price: Number
