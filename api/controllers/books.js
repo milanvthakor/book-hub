8796b1125c71b70a031505c8b57203d5ed57dd1d
Milan Thakor <milanv.thakor@gmail.com>
2023-09-07 13:01:13 +0530

    Add async function handler

--- api/controllers/books.js
+++ api/controllers/books.js
@@ -1,27 +1,25 @@
 const { default: mongoose } = require('mongoose');
 const Book = require('../models/book');
 
-module.exports.books = async (req, res, next) => {
+const asyncHandler = require('../../utils/async-handler');
+
+module.exports.books = asyncHandler(async (req, res, next) => {
     const search = req.query.search || "";
 
-    try {
-        const books = await Book.find({
-            '$or': [
-                { title: { '$regex': search, '$options': 'i' } },
-                { author: { '$regex': search, '$options': 'i' } }
-            ]
-        }).select('-__v');
+    const books = await Book.find({
+        '$or': [
+            { title: { '$regex': search, '$options': 'i' } },
+            { author: { '$regex': search, '$options': 'i' } }
+        ]
+    }).select('-__v');
 
-        res.status(200).json({
-            count: books.length,
-            books: books
-        });
-    } catch (err) {
-        next(err);
-    }
-}
+    res.status(200).json({
+        count: books.length,
+        books: books
+    });
+});
 
-module.exports.bookDetails = async (req, res, next) => {
+module.exports.bookDetails = asyncHandler(async (req, res, next) => {
     const id = req.params.id;
 
     // Validate the book id
@@ -31,17 +29,13 @@ module.exports.bookDetails = async (req, res, next) => {
         return next(err);
     }
 
-    try {
-        // Check if there exists any book with the given id or not
-        const book = await Book.findById(id).select('-__v');
-        if (!book) {
-            const err = new Error('No book found with given id');
-            err.status = 404;
-            return next(err);
-        }
-
-        res.status(200).json(book);
-    } catch (err) {
-        next(err);
+    // Check if there exists any book with the given id or not
+    const book = await Book.findById(id).select('-__v');
+    if (!book) {
+        const err = new Error('No book found with given id');
+        err.status = 404;
+        return next(err);
     }
-}
+
+    res.status(200).json(book);
+});
--- api/controllers/user.js
+++ api/controllers/user.js
@@ -5,7 +5,9 @@ const jwt = require('jsonwebtoken');
 const { isValidEmail, isValidPassword } = require('../../utils/helper');
 const User = require('../models/user');
 
-module.exports.register = async (req, res, next) => {
+const asyncHandler = require('../../utils/async-handler');
+
+module.exports.register = asyncHandler(async (req, res, next) => {
     const { email, password } = req.body;
 
     // Validate email
@@ -22,35 +24,31 @@ module.exports.register = async (req, res, next) => {
         return next(err);
     }
 
-    try {
-        // Check if there exists any user with same email.
-        const user = await User.findOne({ email: email });
-        if (user) {
-            const err = new Error('Email already exists');
-            err.status = 409;
-            return next(err);
-        }
-
-        // Hash password
-        const hashPassword = await bcrypt.hash(password, 10);
-
-        // Create user
-        const newUser = new User({
-            _id: new mongoose.Types.ObjectId(),
-            email: email,
-            password: hashPassword
-        });
-        await newUser.save();
-
-        res.status(201).json({
-            message: 'User created'
-        });
-    } catch (err) {
-        next(err);
+    // Check if there exists any user with same email.
+    const user = await User.findOne({ email: email });
+    if (user) {
+        const err = new Error('Email already exists');
+        err.status = 409;
+        return next(err);
     }
-};
 
-module.exports.login = async (req, res, next) => {
+    // Hash password
+    const hashPassword = await bcrypt.hash(password, 10);
+
+    // Create user
+    const newUser = new User({
+        _id: new mongoose.Types.ObjectId(),
+        email: email,
+        password: hashPassword
+    });
+    await newUser.save();
+
+    res.status(201).json({
+        message: 'User created'
+    });
+});
+
+module.exports.login = asyncHandler(async (req, res, next) => {
     const { email, password } = req.body;
 
     // Validate email
@@ -67,31 +65,27 @@ module.exports.login = async (req, res, next) => {
         return next(err);
     }
 
-    try {
-        // Check if there exists a user with the given email or not.
-        const user = await User.findOne({ email: email });
-        if (!user) {
-            const err = new Error('Login failed');
-            err.status = 401;
-            return next(err);
-        }
-
-        // Compare the passwords.
-        const match = await bcrypt.compare(password, user.password);
-        if (!match) {
-            const err = new Error('Login failed');
-            err.status = 401;
-            return next(err);
-        }
-
-        // Generate JWT authentication token.
-        const token = jwt.sign({ email: email, id: user._id }, process.env.JWT_SECRET_KEY, { expiresIn: '1h' });
-
-        res.status(200).json({
-            message: 'Login successfully',
-            token: token
-        });
-    } catch (err) {
-        next(err);
+    // Check if there exists a user with the given email or not.
+    const user = await User.findOne({ email: email });
+    if (!user) {
+        const err = new Error('Login failed');
+        err.status = 401;
+        return next(err);
     }
-}
+
+    // Compare the passwords.
+    const match = await bcrypt.compare(password, user.password);
+    if (!match) {
+        const err = new Error('Login failed');
+        err.status = 401;
+        return next(err);
+    }
+
+    // Generate JWT authentication token.
+    const token = jwt.sign({ email: email, id: user._id }, process.env.JWT_SECRET_KEY, { expiresIn: '1h' });
+
+    res.status(200).json({
+        message: 'Login successfully',
+        token: token
+    });
+});
